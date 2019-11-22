6487de76948fe83161d2aa61a1973ef60a895b33
Fin <spamsuckersunited@gmail.com>
2019-11-22 16:27:06 -0700

    Fix rendering, better smoothing, colormaps, other stuff

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,65 +1,110 @@
-from math import*
-from colorsys import*
-from sys import*
-from struct import*
+from math import *
+from PIL import Image
+from numpy import zeros, float32, uint8
+from tqdm import *
 from collections import *
+from matplotlib import cm
 
-def mandelbrot(xd,o):
-    xd *= 2
-    yd = 2 * xd // 3
-    pixels = {}
-    pix2 = []
-    histo = defaultdict(lambda:0)
-    for Py in range(yd):
-        for Px in range(xd):
-            x0 = (Px / xd) * 3.0 - 2.0
-            y0 = (Py / yd) * 2.0 - 1.0
-            x, y = 0.0, 0.0
-            k = 0
-            while x * x + y * y <= 4.0 and k < o:
-                xtemp = x*x - y*y + x0
-                y = 2 * x * y + y0
-                x = xtemp
-                k += 1
-            if k < o:
-                zn = log(x * x + y * y) / 2
-                nu = log(zn / log(2)) / log(2)
-                k += 1 - nu
-                histo[floor(k)] += 1
-            pixels[(Px, Py)] = float(k)
 
-    t = sum(histo.values())
-    h = 0
+def lerp(a, b, t):
+    return a * (1 - t) + b * t
+
+
+def _mandelbrot(xD_1, yD_1, x1, x2, y1, y2, maxiter, scale=2, colormap=None):
+    xD = xD_1 * scale
+    yD = yD_1 * scale
+
+    x_scale = (x2 - x1) / xD
+    y_scale = (y2 - y1) / yD
+
+    histogram = defaultdict(lambda: 0)
+
+    values = zeros((yD, xD), float32)
+
+    pixels = zeros((yD, xD, 3), uint8)
+
+    for xy in tqdm(range(yD * xD)):
+        Py, Px = xy // xD, xy % xD
+        x0 = Px * x_scale + x1
+        y0 = Py * y_scale + y1
+
+        zr = zi = tr = ti = k = 0
+
+        while k <= maxiter and tr + ti < 15:
+            zi = 2 * zr * zi + y0
+            zr = tr - ti + x0
+            tr = zr ** 2
+            ti = zi ** 2
+            k += 1
+
+        adj = smooth(maxiter, k, tr, ti)
+
+        values[Py, Px] = adj
+
+        if adj < maxiter:
+            histogram[floor(adj)] += 1
+
+    total = sum(list(histogram.values()))
+
     hues = []
-    for i in range(o):
-        h += histo[i] / t
-        hues.append(h)
-    hues.append(h)
+    accum = 0
+    for i in range(maxiter):
+        accum += histogram[i] / total
+        hues.append(accum)
+    hues.append(accum)
 
-    for Py in range(yd):
-        for Px in range(xd):
-            k = pixels[(Px, Py)]
-            h = max(1 - (hues[floor(k)] * (1 - (k % 1)) + hues[ceil(k)] * (k % 1)), 0.0)
+    for xy in tqdm(range(yD * xD)):
+        Py, Px = xy // xD, xy % xD
 
-            pix2.append(col(h, 1.0, 1.0 if k < o else 0))
-    targa(pix2, xd, yd)
+        adj = values[Py, Px].item()
+        interp = 1 - lerp(hues[floor(adj)], hues[ceil(adj)], adj % 1)
 
+        pixels[Py, Px] = tuple(map(
+            lambda x: floor(x * 255),
+            [0, 0, 0] if adj >= maxiter else color(interp, colormap)
+        ))
 
-# HSV to RGB.
-def col(h,s,v):
-    i=int(h*6)
-    f,p=h*6-i,v*(1-s)
-    q,t=v*(1-s*f),v*(1-s*(1-f))
-    l=[(v,t,p),(q,v,p),(p,v,t),(p,v,q),(t,p,v),(v,p,q)][i%6]
-    return list(floor(x * 255) for x in l)
-
-
-def targa(pixels, w, h):
-    f=open('out.tga', 'wb')
-    header = b"\x00\x00\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00" + pack("<BBBB", 255 & w, 255 & (w >> 8), 255 & h, 255 & (h >> 8)) + b"\x18\x00"
-    f.write(header)
-    for pixel in pixels:
-        f.write(pack("<BBB", *pixel))
+    img = Image.fromarray(pixels, 'RGB')
+    img = img.resize((xD_1, yD_1), Image.LANCZOS)
+    img.save('mandeltest.png')
+    img.show()
 
 
-mandelbrot(3000, 60)
+def color(interp, map=None):
+    if map is None:
+        return [interp, 1, 1]
+    else:
+        return cm.get_cmap(map)(clamp(interp))[:3]
+
+
+lb = 1 / log(2)
+hlb = log(0.5) * lb
+
+
+def clamp(x, min=0, max=1):
+    if x > max:
+        return max
+    if x < min:
+        return min
+    return x
+
+
+def smooth(maxiter, k, tr, ti):
+    if tr + ti > 1.0:
+        c = log(log(tr + ti))
+    else:
+        c = 0
+    return clamp((5 + k - hlb - c * lb), 0, maxiter)
+    # return (5 + k - hlb - log(log(tr + ti)) * lb) / maxiter
+
+
+def easybrot(xd, maxiter, colormap='twilight'):
+    yd = floor(xd * 2 / 3)
+    return _mandelbrot(xd, yd, -2, 1, -1, 1, maxiter, 2, colormap)
+
+
+# easybrot(300, 30)
+
+easybrot(3000, 130)
+
+# easybrot(1500, 1000, 100)
